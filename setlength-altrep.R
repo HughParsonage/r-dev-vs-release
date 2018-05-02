0fbba76d1382cebf4b6cfb2e4c6ac7022f51e98f
HughParsonage <hugh.parsonage@gmail.com>
2018-05-02 17:43:19 +1000

    Gotta be minimal

--- setlength-altrep.R
+++ setlength-altrep.R
@@ -1,24 +1,22 @@
 library(data.table)
 
-parse_tex0 <- function(tex_lines) {
-  out <- data.table(tex_group = c(0L, 1L, 1L))
-  seq_max_tex_group <- seq_len(5)
-
-
-  out[, z1 := .GRP, by = "tex_group"]
-  out[tex_group == 0L]
-  out[, z2 := .GRP, by = "tex_group"]
-
-  tg <- sprintf("tg%s", seq_max_tex_group)
-  GROUP_IDz <- sprintf("GROUP_ID%s", seq_max_tex_group)
-  setindexv(out, "tex_group")
-  for (j in seq_len(5)) {
-    tgj <- tg[j]
-    out[, (tgj) := 1L]
-    GROUP_IDj <- GROUP_IDz[j]
-    out[tex_group == j, (GROUP_IDj) := .GRP, by = c(tgj)]
-  }
-  out
-}
-parse_tex0()
+# parse_tex0 <- function(tex_lines) {
+#   out <- data.table(tex_group = c(0L, 1L, 1L))
+#   out[, z1 := .GRP, by = "tex_group"]
+#   out[tex_group == 0L]
+#   out[, z2 := .GRP, by = "tex_group"]
+#
+#   tg <- sprintf("tg%s", seq_max_tex_group)
+#   GROUP_IDz <- sprintf("GROUP_ID%s", seq_max_tex_group)
+#   setindexv(out, "tex_group")
+#   for (j in seq_len(5)) {
+#     tgj <- tg[j]
+#     out[, (tgj) := 1L]
+#     GROUP_IDj <- GROUP_IDz[j]
+#     out[tex_group == j, (GROUP_IDj) := .GRP, by = c(tgj)]
+#   }
+#   out
+# }
+out <- data.table(tex_group = c(0L, 1L, 1L))
+out[, z1 := .GRP, by = "tex_group"]
 
