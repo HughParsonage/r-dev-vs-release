a5f4aeb239436f255871b1ff97f0cab6304cdc14
HughParsonage <hugh.parsonage@gmail.com>
2018-08-10 00:07:48 +1000

    Try a bit longer

--- grattan-benchmarks.R
+++ grattan-benchmarks.R
@@ -4,7 +4,7 @@ if (requireNamespace("grattan", quietly = TRUE) &&
 library(grattan)
   set.seed(19842014)
   from_fys10K <- sample(yr2fy(1984:2014), size = 10e3, replace = TRUE)
-  from_fys100M <- rep(from_fys10K, times = 1e6/10e3)
+  from_fys100M <- rep(from_fys10K, times = 10e6/10e3)
   print(bench::system_time(cpi_inflator(from_fy = from_fys100M,
                                                 to_fy = "2015-16",
                                                 adjustment = "none")))
