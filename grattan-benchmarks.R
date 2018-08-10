6b291257a1a4592add5bc5c5320e669f30617db3
HughParsonage <hugh.parsonage@gmail.com>
2018-08-10 22:45:05 +1000

    Add benches

--- grattan-benchmarks.R
+++ grattan-benchmarks.R
@@ -1,7 +1,14 @@
 
 if (requireNamespace("grattan", quietly = TRUE) &&
-	  requireNamespace("bench", quietly = TRUE)) {
+	  requireNamespace("bench", quietly = TRUE) &&
+    requireNamespace("taxstats", quietly = TRUE)) {
 library(grattan)
+  
+  cat("sample_file_1314:\n")
+  print(bench::system_time(sample_file_1314[, income_tax(Taxable_Income, "2013-14", .dots.ATO = .SD)]))
+  cat("sample_file_1314 (hot):\n")
+  print(bench::system_time(sample_file_1314[, income_tax(Taxable_Income, "2013-14", .dots.ATO = .SD)]))
+
   set.seed(19842014)
   from_fys10K <- sample(yr2fy(1984:2014), size = 10e3, replace = TRUE)
   from_fys100M <- rep(from_fys10K, times = 10e6/10e3)
@@ -21,6 +28,9 @@ library(grattan)
   print(bench::system_time(cpi_inflator(from_fy = from_fys100M,
                                                 to_fy = "2015-16",
                                                 adjustment = "none")))
+
+
+  rm(from_fys100M)
 
 } else {
 	cat("\nUnable to record benchmark.\n")
