66a7e0ddcbde7de0019c69aa39ba0e04bab0a022
HughParsonage <hugh.parsonage@gmail.com>
2018-08-26 13:34:05 +1000

    Would be 14GB memory consumption

--- large-grattan-benchmarks.R
+++ large-grattan-benchmarks.R
@@ -20,7 +20,7 @@ if (requireNamespace("grattan", quietly = TRUE, lib.loc = tempf) &&
   gc()
 
   set.seed(19842014)
-  from_fys1G <- sample(yr2fy(1984:2016), size = 1000, replace = TRUE)
+  from_fys1G <- sample(yr2fy(1984:2016), size = 100, replace = TRUE)
   cat(".")
   from_fys1G <- rep(from_fys1G, times = 50e3)
   cat("cpi ", prettyNum(length(from_fys1G), big.mark = ","), "\n")
@@ -33,6 +33,12 @@ if (requireNamespace("grattan", quietly = TRUE, lib.loc = tempf) &&
   devtools::install_github('hughparsonage/grattan', force = TRUE, quiet = TRUE, args = paste0('--library="', normalizePath(tmp, winslash = "/"), '"'))
   report_version(tmp)
   library("grattan", lib.loc = tmp)
+  cat("cpi ", prettyNum(length(from_fys1G), big.mark = ","), "\n")
+  print(bench::system_time(cpi_inflator(from_fy = from_fys1G,
+                                                to_fy = "2015-16",
+                                                adjustment = "none")))
+
+  from_fys1G <- rep(from_fys1G, times = 10)
   cat("cpi ", prettyNum(length(from_fys1G), big.mark = ","), "\n")
   print(bench::system_time(cpi_inflator(from_fy = from_fys1G,
                                                 to_fy = "2015-16",
