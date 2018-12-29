fa294437e2555ca2a73fd0a9bddbfa3309ac663c
HughParsonage <hugh.parsonage@gmail.com>
2018-12-29 21:21:59 +1100

    Need library()

--- datatable2397.R
+++ datatable2397.R
@@ -1,15 +1,15 @@
 # datatable2397.R
 if (unname(Sys.info()["sysname"] == "Linux")) {
+	detach("package:data.table", unload = TRUE)
 remove.packages("data.table")
-fread("https://github.com/HughParsonage/US-flights-data-2017/raw/master/data-raw/ReleaseableAircraft2017/ACFTREF.txt")
 install.packages("data.table")
+library(data.table)
+fread("https://github.com/HughParsonage/US-flights-data-2017/raw/master/data-raw/ReleaseableAircraft2017/ACFTREF.txt")
 
-
+detach("package:data.table", unload = TRUE)
 remove.packages("data.table")
 devtools::install_github("rdatatable/data.table")
 library(data.table)
-
-
 fread("https://github.com/HughParsonage/US-flights-data-2017/raw/master/data-raw/ReleaseableAircraft2017/ACFTREF.txt")
 }
 
