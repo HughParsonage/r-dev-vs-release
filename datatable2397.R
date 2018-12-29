04d83263035a09e66171a90f04d6111adcff8cd1
HughParsonage <hugh.parsonage@gmail.com>
2018-12-29 21:08:55 +1100

    See if current version works

--- datatable2397.R
+++ datatable2397.R
@@ -1,5 +1,10 @@
 # datatable2397.R
 remove.packages("data.table")
+fread("https://github.com/HughParsonage/US-flights-data-2017/raw/master/data-raw/ReleaseableAircraft2017/ACFTREF.txt")
+install.packages("data.table")
+
+
+remove.packages("data.table")
 devtools::install_github("rdatatable/data.table")
 library(data.table)
 
