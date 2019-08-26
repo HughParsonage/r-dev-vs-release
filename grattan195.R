187fbd32765bc04f0fda104036319dc6ad7a2bf1
HughParsonage <hugh.parsonage@gmail.com>
2019-08-26 19:14:51 +1000

    Need both, don't crossjoin

--- grattan195.R
+++ grattan195.R
@@ -1,6 +1,9 @@
 
+	Sys.setenv('R_GRATTAN_BUILD_MAIN_VIGNETTE' = 'true')
+	Sys.setenv('R_GRATTAN_ALLOW_TAXSTATS' = 'true')
 install.packages("remotes", repos = "https://cran.rstudio.com")
 install.packages("grattan", repos = "https://cran.rstudio.com")
+
 grattan::install_taxstats(type = "source")
 remotes::install_github("hughparsonage/grattan", dependencies = TRUE)
 
